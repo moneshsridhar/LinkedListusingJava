d86d9e5bcd070b7c3389412f6560729626ca3174
Monesh Sridhar <46098323+moneshsridhar@users.noreply.github.com>
2019-12-07 15:07:50 +0530

    Inserting elements in Linked List.

---
+++ LinkedList/src/linkedlist/MainClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist;
+
+/**
+ *
+ * @author monesh
+ */
+public class MainClass 
+{
+    public static void main(String[] args)
+    {
+        LinkedList list = new LinkedList();
+        
+        list.insert(3);
+        list.insert(6);
+        list.insert(9);
+        
+        list.show();
+    }
+}
